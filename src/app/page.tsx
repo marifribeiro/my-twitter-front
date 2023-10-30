25344771694b496de468781d9a3f29d19b09359d
Mariana Ribeiro <mariana-fribeiro@live.com>
2023-10-30 15:17:52 -0300

    testing commit hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,12 @@ import Image from "next/image"
 import styles from './page.module.css'
 
 export default function Home() {
-const banana = 'bananinha'
-let caqui = 'caqui'
-
+	const banana = 'banana'
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
 				<p>
 					{banana}
-					{caqui}
 					Get started by editing&nbsp;
 					<code className={styles.code}>src/app/page.tsx</code>
 				</p>
