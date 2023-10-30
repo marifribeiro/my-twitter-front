55f5ded0cca8ef31b41b9903ea3ad58339b8be8f
Mariana Ribeiro <mariana-fribeiro@live.com>
2023-10-30 10:13:55 -0300

    testing pre commit hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from './page.module.css'
 
 export default function Home() {
-	let babana = "banana";
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
