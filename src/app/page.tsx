889bd568a4ed26f8813ba384e5c063e6bf4a9aea
Mariana Ribeiro <mariana-fribeiro@live.com>
2023-10-30 15:10:24 -0300

    testing pre commit hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
 const banana = 'bananinha'
+
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
